41f1add7dbc244b09e1c523a4fbdc8dd1401e0f7
Gian Antonio Pura <gianantonio.pura@itsrizzoli.it>
2023-12-18 11:00:30 +0100

    input utente

--- src/Binary.java
+++ src/Binary.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Binary {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci una stringa binaria: ");
+        String Binary = scanner.nextLine();
+
+        int decimalValue = 0;
+        
+        System.out.println("Valore decimale: " + decimalValue);
+        scanner.close();
     }
 }
