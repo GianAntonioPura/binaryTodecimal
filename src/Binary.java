abc70db01c92797685b0952619f5d963cdca55ae
Gian Antonio Pura <gianantonio.pura@itsrizzoli.it>
2023-12-18 11:40:25 +0100

    conversione

--- src/Binary.java
+++ src/Binary.java
@@ -2,4 +2,15 @@ public class Binary {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    public static int Bynary(String binaryString) {
+        int decimaValue = 0;
+        int len = binaryString.length();
+
+       for (int i=0; i < len; i++) {
+            int digit = Character.getNumericValue(binaryString.charAt(i));
+            int power = len - i - 1;
+            decimaValue = decimaValue + (int) (digit * Math.pow(2, power));
+        }
+        return decimaValue;
+    }
 }
